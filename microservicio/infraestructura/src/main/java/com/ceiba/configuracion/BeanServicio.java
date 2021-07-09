aaff4c2eea69c5b8e93529323e015791f44a7a21
Franklin José Vasquez Villar - Ceiba Software <franklin.vasquez@ceiba.com.co>
2021-07-09 00:13:12 -0500

    eliminar todo el esquema y clases del usuario

--- microservicio/infraestructura/src/main/java/com/ceiba/configuracion/BeanServicio.java
+++ microservicio/infraestructura/src/main/java/com/ceiba/configuracion/BeanServicio.java
@@ -2,37 +2,18 @@ package com.ceiba.configuracion;
 
 import com.ceiba.usuario.puerto.repositorio.RepositorioCita;
 import com.ceiba.usuario.puerto.repositorio.RepositorioPersona;
-import com.ceiba.usuario.puerto.repositorio.RepositorioUsuario;
 import com.ceiba.usuario.servicio.ServicioActualizarCita;
 import com.ceiba.usuario.servicio.ServicioActualizarPersona;
-import com.ceiba.usuario.servicio.ServicioActualizarUsuario;
 import com.ceiba.usuario.servicio.ServicioCrearCita;
 import com.ceiba.usuario.servicio.ServicioCrearPersona;
-import com.ceiba.usuario.servicio.ServicioCrearUsuario;
 import com.ceiba.usuario.servicio.ServicioEliminarCita;
 import com.ceiba.usuario.servicio.ServicioEliminarPersona;
-import com.ceiba.usuario.servicio.ServicioEliminarUsuario;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BeanServicio {
 
-    @Bean
-    public ServicioCrearUsuario servicioCrearUsuario(RepositorioUsuario repositorioUsuario) {
-        return new ServicioCrearUsuario(repositorioUsuario);
-    }
-
-    @Bean
-    public ServicioEliminarUsuario servicioEliminarUsuario(RepositorioUsuario repositorioUsuario) {
-        return new ServicioEliminarUsuario(repositorioUsuario);
-    }
-
-    @Bean
-    public ServicioActualizarUsuario servicioActualizarUsuario(RepositorioUsuario repositorioUsuario) {
-        return new ServicioActualizarUsuario(repositorioUsuario);
-    }
-	
     @Bean
     public ServicioCrearCita servicioCrearCita(RepositorioCita repositorioCita) {
     	return new ServicioCrearCita(repositorioCita);
